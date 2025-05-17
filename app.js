c34eda30220bf5f10d09a113163fb8f2fd6d2c08
inayat khan <inayatahmedkhan00@gmail.com>
2025-05-17 19:52:30 +0530

    replace ul content completed

--- app.js
+++ app.js
@@ -1,53 +1,98 @@
-import express from "express"
-import { link } from "fs"
-import { readFile, writeFile } from "fs/promises"
-import path from "path"
 
+import {readFile,writeFile} from 'fs/promises'
+
+import path from 'path'
+import crypto from "crypto";
+import express from "express"
 
 
 
 const app = express()
-const PORT = 3003
-// path of JSON sile
-const DATA_file = path.join("data","links.json")
 
-app.use(express.static("public"))
+const PORT =   3003;
+const DATA_FILE = path.join("data","links.json");
 
-const loadLinks = async () => {
-  try {
-    const data = await readFile(DATA_file, "utf-8");
-         return JSON.parse(data)
-        
+// app.use(express.static("views"))
+app.use(express.urlencoded({extended:true}))
 
-         
-           
-  } catch (err) {
-    console.error("Error reading file:", err);
-  }
+
+
+const loadLinks = async()=>{
+    try {
+        const data = await readFile(DATA_FILE,"utf8")
+        return JSON.parse(data)
+    } catch (error) {
+        if(error.code === "ENOENT"){
+            await writeFile(DATA_FILE,JSON.stringify({}))
+            return {}
+             }
+
+             throw error
+    }
 };
 
-
-
-
-
-
-app.get("/",async(req,res)=>{
-try {
-   const file = path.join(import.meta.dirname,"public","index.html")
-   const links = await loadLinks()
-   const content = file.toString().replaceAll("{{shorten_urls}}","hello")
-   
-  await res.sendFile(file)
-} catch (error) {
-    
+const saveLinks = async (links)=>{
+    await writeFile(DATA_FILE,JSON.stringify(links))
 }
 
+// use get method with express
+app.get("/",async(req,res)=>{
+try {
+    const file = await readFile(path.join(import.meta.dirname,"public","index.html"))
+    const links = await loadLinks()
+  const content = file.toString().replaceAll("{{shorten_urls}}",
+    Object.entries(links).map(
+        ([shortCode,url])=>
+            `<li> <a href="/${shortCode}" target="_blank" class="text-blue-700"> ${req.host}/${shortCode}</a> - ${url} </li>`
+    ).join("")
+);
+return res.send(content)
+
+} catch (error) {
+    console.error(or)
+    return res.status(500).send("Internal Server error")
+}
 })
+
+
+// use post method with express
+app.post("/",async (req,res)=>{
+try {
+   const {url,shortCode} = req.body 
+   const finalShortcode = shortCode || crypto.randomBytes(4).toString("hex")
+   const links = await loadLinks();  
+    if(links[finalShortcode]){
+        return res.status(400).send("Short code already exists. please choose another.");
+    }
+   
+    links[finalShortcode] = url
+  await saveLinks(links) 
+  return res.redirect("/")       
+   
+} catch (error) {
+    console.error(error)
+    return res.status(500).send("Internal server error")
+}
+})
+
+app.get("/:shortCode",async(req,res)=>{
+try {
+    const {shortCode} = req.params
+    const links = await loadLinks()
+    if(!links[shortCode]) return res.status(404).send("404 error occured")
+    return res.redirect(links[shortCode])
+  
+} catch (error) {
+    console.error(error)
+    return res.status(500).send("Internal Server error")
+}
+})
+
+
+
 
 
 
 app.listen(PORT,()=>{
-
-    console.log(`server is running at http://localhost:${PORT}`)
+    console.log(` server running at http://localhost:${PORT} `)
 })
-
