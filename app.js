30df32d2ea6e733475dd2b7d9fe3157784142989
inayat khan <inayatahmedkhan00@gmail.com>
2025-05-17 22:10:19 +0530

    completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import express from "express"
+import express, { urlencoded } from "express"
 
 import { readFile, writeFile } from "fs/promises"
+
 import path from "path"
 
 
@@ -12,15 +13,25 @@ const PORT = 3003
 const DATA_file = path.join("data","links.json")
 
 // app.use(express.static("public"))
+app.use(urlencoded({"extended":true}))
 
 const loadLinks = async () => {
   try {
     const data = await readFile(DATA_file, "utf-8");
          return JSON.parse(data)
-        } catch (err) {
-    console.error("Error reading file:", err);
+        } catch (error) {
+      if(error.code === "ENOENT"){
+            await writeFile(DATA_FILE,`${JSON.stringify({})}`)
+            return {}
+             }
+
+             throw error
   }
 };
+
+const saveLinks = async(links)=>{
+await writeFile(DATA_file,JSON.stringify(links))
+}
 
 
 
@@ -46,6 +57,66 @@ return res.send(content)
 }
 
 })
+
+// post method
+
+app.post("/", async(req,res)=>{
+try {
+   const {url,shortCode} = req.body 
+   const finalShortcode = shortCode || crypto.randomBytes(4).toString("hex")
+   const links = await loadLinks();  
+    if(links[finalShortcode]){
+        return res.status(400).send("Short code already exists. please choose another.");
+    }
+   
+    links[finalShortcode] = url
+  await saveLinks(links) 
+  return res.redirect("/")    
+
+    
+} catch (error) {
+    console.error(error)
+    return res.status(500).send("Internal server error")
+} 
+
+})
+
+// app.post("/",async (req,res)=>{
+// try {
+//    const {url,shortCode} = req.body 
+//    const finalShortcode = shortCode || crypto.randomBytes(4).toString("hex")
+//    const links = await loadLinks();  
+//     if(links[finalShortcode]){
+//         return res.status(400).send("Short code already exists. please choose another.");
+//     }
+   
+//     links[finalShortcode] = url
+//   await saveLinks(links) 
+//   return res.redirect("/")       
+   
+// } catch (error) {
+//     console.error(error)
+//     return res.status(500).send("Internal server error")
+// }
+// })
+
+
+
+
+
+app.get("/:shortCode",async(req,res)=>{
+try {
+    const {shortCode} = req.params
+    const links = await loadLinks()
+    if(!links[shortCode]) return res.status(404).send("404 error occured")
+    return res.redirect(links[shortCode])
+  
+} catch (error) {
+    console.error(error)
+    return res.status(500).send("Internal Server error")
+}
+})
+
 
 
 
