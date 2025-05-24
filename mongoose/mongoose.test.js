97b439a4c4975b5e78d854e7eac2119c45511d41
inayat khan <inayatahmedkhan00@gmail.com>
2025-05-25 03:42:29 +0530

    now connect with data base

--- mongoose/mongoose.test.js
+++ mongoose/mongoose.test.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+// now we are creating connection with mangoose
+try {
+    // syntx (mangdbURL/databasename)
+    await mongoose.connect("mongodb://localhost:27017/mongoose_database")
+
+} catch (error) {
+    console.error(error)
+    // process exit mean if error will come then run way there
+    process.exit()
+}
