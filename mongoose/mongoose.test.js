248656fe1b333bb089acb9242365b0ca0fba8e1e
inayat khan <inayatahmedkhan00@gmail.com>
2025-05-25 04:12:22 +0530

    now mangoose with mangoDB connection completed

--- mongoose/mongoose.test.js
+++ mongoose/mongoose.test.js
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-// now we are creating connection with mangoose
+
+// step 1: now we are creating connection with mangoose
 try {
     // syntx (mangdbURL/databasename)
     await mongoose.connect("mongodb://localhost:27017/mongoose_database");
-    
+
     // if we will give debug true then mangodb give us error reason.
     mongoose.set("debug",true);
 
@@ -13,3 +14,21 @@ try {
     // process exit mean if error will come then run way there
     process.exit()
 }
+
+// step 2 : create schema
+const userSchema = mongoose.Schema({
+    // and we should type first latter in chapital 
+name:{type: String,required:true},
+email:{type: String, required: true, unique:true},
+age : {type:Number,required:true , min:5},
+createdAt : {type:Date, default: Date.now() }
+})
+
+// step 3 : creating a model
+// we show pass two parameter in model (this is collection name,ourSchema)
+const Users =  mongoose.model("user",userSchema)
+
+await Users.create({name:"roman",age:26,email:"romanrg@gmail.com"})
+
+// for closing method
+await mongoose.connection.close()
